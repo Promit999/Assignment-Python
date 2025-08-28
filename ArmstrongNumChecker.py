cbbfd9738e87af360bf3d38ad9493a4ce13118dd
Promit999 <106440846+Promit999@users.noreply.github.com>
2025-08-28 22:08:49 +0200

    Add files via upload

---
+++ ArmstrongNumChecker.py
@@ -0,0 +1,21 @@
+x = int(input("Enter the number: \n"))
+countx = x
+original = x
+summ = 0
+count = 0
+while countx > 0:
+    countx = countx // 10
+    count +=1
+#n = len(str(x))
+if countx == 0:
+    while x > 0:
+        ld = x % 10
+        print("last digit: " + str(ld))
+        summ += ld ** count
+        print("sum: " + str(summ) + " count: " + str(count))
+        x = x // 10
+        print("x: " + str(x))
+    if summ == original:
+        print("the number: " + str(original) + " is an armstrong number")
+    elif summ != original:
+        print("the number: " + str(original) + " is not an armstrong number")
